df27881288d6942fffc9169e9c33c2130dc03f79
pineapplebin <pineapplewing@126.com>
2018-08-12 16:39:08 +0800

    clean up master

--- src/components/CardShows/CardShowMixin.js
+++
@@ -1,7 +0,0 @@
-export default {
-  methods: {
-    getCard () {
-      return this.card;
-    }
-  }
-}
--- src/plugins/effect/index.js
+++
@@ -1,9 +0,0 @@
-import render from './render.js'
-
-export default {
-  install(Vue) {
-    Vue.prototype.$$ability = {
-      translate: render
-    }
-  }
-}
